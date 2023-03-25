96a8ce03cda71b298afc20795e5e080a5fe5f79f
Ichi Zamboni <ichizamboni@Ichis-MacBook-Pro.local>
2023-03-25 15:53:11 -0700

    add block07 files from vscode to github repo

---
+++ index.js
@@ -0,0 +1,19 @@
+// This will be my first practice in Javascript code.
+// create three variables using three mathematical calculations to generate the three codes in a combination
+console.log("Hello World, Welcome to the Secret Vault!");
+window.alert(
+  "You have received this message because you have been chosen to open an important vault."
+);
+let var1 = "10";
+console.log("what is 5+5=" + var1);
+alert("10");
+
+let var2 = "40";
+console.log("what is 20x2=" + var2);
+alert("40");
+
+let var3 = "39";
+console.log("what is 78/2=" + var3);
+alert("39");
+
+alert("You've entered the secret combination!");
